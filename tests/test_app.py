2c619b5c148b0d2e075e5172e21ffa9abd28bad1
lucas <lucasemmanuel2005@gmail.com>
2024-06-16 16:59:49 -0300

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
